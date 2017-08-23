81ab36ce460002d1463e82a14b575d60f81c8fa6
dev-jangwon <dev.jangwon@gmail.com>
2017-08-24 00:16:01 +0900

    Project Upload

---
+++ src/realtimes/controller/TestController.java
@@ -0,0 +1,38 @@
+package realtimes.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestController
+ */
+@WebServlet("/TestController")
+public class TestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @return 
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("RealTimes");
+	}
+
+}
